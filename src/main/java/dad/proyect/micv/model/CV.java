4e585880da796b7b7e8482d50544f4b25b5e8676
SamirElKharrat <samirelkharrat@gmail.com>
2020-11-30 15:03:53 +0000

    .

--- src/main/java/dad/proyect/micv/model/CV.java
+++ src/main/java/dad/proyect/micv/model/CV.java
@@ -3,24 +3,20 @@ package dad.proyect.micv.model;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
-public class cv {
+public class CV {
+
 	private ObjectProperty<Personal> personal = new SimpleObjectProperty<Personal>(new Personal());
 
 	public final ObjectProperty<Personal> personalProperty() {
 		return this.personal;
 	}
-	
 
 	public final Personal getPersonal() {
 		return this.personalProperty().get();
 	}
-	
 
 	public final void setPersonal(final Personal personal) {
 		this.personalProperty().set(personal);
 	}
 	
-	
-	
-
 }
--- src/main/java/dad/proyect/micv/model/nacionalidad.java
+++ src/main/java/dad/proyect/micv/model/nacionalidad.java
@@ -1,24 +1,26 @@
 package dad.proyect.micv.model;
-
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class nacionalidad {
 	private StringProperty denominacion = new SimpleStringProperty();
+	
+	public nacionalidad() {}
+	
+	public nacionalidad(String denominacion) {
+		setDenominacion(denominacion);
+	}
 
 	public final StringProperty denominacionProperty() {
 		return this.denominacion;
 	}
-	
 
 	public final String getDenominacion() {
 		return this.denominacionProperty().get();
 	}
-	
 
 	public final void setDenominacion(final String denominacion) {
 		this.denominacionProperty().set(denominacion);
 	}
-	
 
 }
