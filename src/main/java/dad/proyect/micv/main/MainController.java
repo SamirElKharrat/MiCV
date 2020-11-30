9d031a27b53fbaa2f184a97fa0f229563840d394
SamirElKharrat <samirelkharrat@gmail.com>
2020-11-30 15:05:17 +0000

    .

--- src/main/java/dad/proyect/micv/main/MainController.java
+++ src/main/java/dad/proyect/micv/main/MainController.java
@@ -1,18 +1,40 @@
 package dad.proyect.micv.main;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.google.gson.JsonSyntaxException;
+
+import dad.proyect.micv.model.CV;
+import dad.proyect.micv.personal.PersonalController;
+import dad.proyect.micv.util.JSONUtils;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class MainController implements Initializable {
-
+	
+	// controllers
+	
+	private PersonalController personalController = new PersonalController();
+	// TODO resto de controladores
+	
+	// model
+	
+	private ObjectProperty<CV> cv = new SimpleObjectProperty<>();
+	
+	// view
+	
     @FXML
     private BorderPane view;
 
@@ -31,13 +53,68 @@ public class MainController implements Initializable {
     @FXML
     private Tab conocimientosTab;
 
+	public MainController() throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+		loader.setController(this);
+		loader.load();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		personalTab.setContent(personalController.getView());
+		
+		cv.addListener((o, ov, nv) -> onCVChanged(o, ov, nv));
+		
+		cv.set(new CV());
+	}
+	
+	private void onCVChanged(ObservableValue<? extends CV> o, CV ov, CV nv) {
+
+		if (ov != null) {
+			personalController.personalProperty().unbind();
+			// TODO desbindear el resto de controladores
+		}
+		
+		if (nv != null) {
+			personalController.personalProperty().bind(nv.personalProperty());
+			// TODO bindear el resto de controladores
+		}
+		
+	}
+
+	public BorderPane getView() {
+		return view;
+	}
+
     @FXML
     void onAbrirAction(ActionEvent event) {
+
+    	FileChooser fileChooser = new FileChooser();
+    	fileChooser.setTitle("Abrir un currículum");
+    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Currículum (*.cv)", "*.cv"));
+    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Todos los archivos (*.*)", "*.*"));
+    	File cvFile = fileChooser.showOpenDialog(App.getPrimaryStage());
+    	if (cvFile != null) {
+    		
+    		try {
+    			cv.set(JSONUtils.fromJson(cvFile, CV.class));
+    			App.info("Se ha abierto el fichero " + cvFile.getName() + " correctamente.", "Pues eso...");
+			} catch (JsonSyntaxException|IOException e) {
+				App.error("Ha ocurrido un error al abrir " + cvFile, e.getMessage());
+			}
+    		
+    	}
+    	
+    }
+
+    @FXML
+    void onAcercaDeAction(ActionEvent event) {
 
     }
 
     @FXML
-    void onAcercaAction(ActionEvent event) {
+    void onCerrarAction(ActionEvent event) {
 
     }
 
@@ -49,22 +126,27 @@ public class MainController implements Initializable {
     @FXML
     void onGuardarComoAction(ActionEvent event) {
 
+	    	FileChooser fileChooser = new FileChooser();
+	    	fileChooser.setTitle("Guardar un currículum");
+	    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Currículum (*.cv)", "*.cv"));
+	    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Todos los archivos (*.*)", "*.*"));
+	    	File cvFile = fileChooser.showSaveDialog(App.getPrimaryStage());
+	    	if (cvFile != null) {
+
+	    		try {
+	    			JSONUtils.toJson(cvFile, cv.get());
+				} catch (JsonSyntaxException|IOException e) {
+					App.error("Ha ocurrido un error al guardar " + cvFile, e.getMessage());
+				}
+	    		
+	    	}
+    	
     }
 
     @FXML
     void onNuevoAction(ActionEvent event) {
-
+    	System.out.println("Nuevo");
+    	cv.set(new CV());
     }
-
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
-	
-
-}
-
-	public BorderPane getView() {
-		return view;
-	}
-	
+    
 }
