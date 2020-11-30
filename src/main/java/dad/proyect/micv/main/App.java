8ceaee94b371e48efd2965f0c2427cae68deb26c
SamirElKharrat <samirelkharrat@gmail.com>
2020-11-30 15:05:33 +0000

    añadimos error e info para tener las alertas preparadas

--- src/main/java/dad/proyect/micv/main/App.java
+++ src/main/java/dad/proyect/micv/main/App.java
@@ -3,29 +3,64 @@ package dad.proyect.micv.main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
+	
+	private static Stage primaryStage;
 	
 	private MainController controller;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-        controller = new MainController();
+		App.primaryStage = primaryStage;
 		
-		Scene scene = new Scene(controller.getView(), 642, 512);
-		primaryStage.setScene(scene);
+		controller = new MainController();
+		
+		Scene escena = new Scene(controller.getView());
+		
+		primaryStage.setScene(escena);
 		primaryStage.setTitle("MiCV");
 		primaryStage.getIcons().add(new Image("/images/cv64x64.png"));
 		primaryStage.show();
+	}
+	
+	public static void error(String header, String content) {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.initOwner(primaryStage);
+		alert.setTitle("Error");
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.showAndWait();
+	}
+	
+	public static void info(String header, String content) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.initOwner(primaryStage);
+		alert.setTitle("Información");
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.showAndWait();
+	}
+	
+	public static Stage getPrimaryStage() {
+		return primaryStage;
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-		
-	}
 
 }
+
