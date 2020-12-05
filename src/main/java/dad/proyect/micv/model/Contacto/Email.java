b4596dbe9f38963b2b5eb049f08ebce29365a916
SamirElKharrat <samirelkharrat@gmail.com>
2020-12-05 18:21:43 +0000

    .

--- src/main/java/dad/proyect/micv/model/Contacto/Email.java
+++ src/main/java/dad/proyect/micv/model/Contacto/Email.java
@@ -7,14 +7,23 @@ public class Email {
 
 	public StringProperty direccion = new SimpleStringProperty();
 
-	public StringProperty getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(StringProperty direccion) {
-		this.direccion = direccion;
+	public final StringProperty direccionProperty() {
+		return this.direccion;
 	}
 	
+
+	public final String getDireccion() {
+		return this.direccionProperty().get();
+	}
+	
+
+	public final void setDireccion(final String direccion) {
+		this.direccionProperty().set(direccion);
+	}
+	
+	
+	
+
 	
 	
 }
