db6f9c639366b6a97099363d6526ca89013b0f47
SamirElKharrat <samirelkharrat@gmail.com>
2020-12-05 22:36:53 +0000

    Añadimos en el CV a formacion y experiencia

--- src/main/java/dad/proyect/micv/model/CV.java
+++ src/main/java/dad/proyect/micv/model/CV.java
@@ -1,13 +1,21 @@
 package dad.proyect.micv.model;
 
 import dad.proyect.micv.model.Contacto.Contacto;
+import dad.proyect.micv.model.Personal.Personal;
+import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class CV {
 
 	private ObjectProperty<Personal> personal = new SimpleObjectProperty<Personal>(new Personal());
 	private ObjectProperty<Contacto> contacto = new SimpleObjectProperty<Contacto>(new Contacto());
+	private ListProperty<Titulo> titulo = new SimpleListProperty<Titulo>(FXCollections.observableArrayList());
+	private ListProperty<Experiencia> experiencia = new SimpleListProperty<Experiencia>(FXCollections.observableArrayList());
 
 	public final ObjectProperty<Personal> personalProperty() {
 		return this.personal;
@@ -34,8 +42,34 @@ public class CV {
 	public final void setContacto(final Contacto contacto) {
 		this.contactoProperty().set(contacto);
 	}
+
+	public final ListProperty<Titulo> tituloProperty() {
+		return this.titulo;
+	}
 	
+
+	public final ObservableList<Titulo> getTitulo() {
+		return this.tituloProperty().get();
+	}
 	
+
+	public final void setTitulo(final ObservableList<Titulo> titulo) {
+		this.tituloProperty().set(titulo);
+	}
+
+	public final ListProperty<Experiencia> experienciaProperty() {
+		return this.experiencia;
+	}
 	
+
+	public final ObservableList<Experiencia> getExperiencia() {
+		return this.experienciaProperty().get();
+	}
+	
+
+	public final void setExperiencia(final ObservableList<Experiencia> experiencia) {
+		this.experienciaProperty().set(experiencia);
+	}
+
 	
 }
