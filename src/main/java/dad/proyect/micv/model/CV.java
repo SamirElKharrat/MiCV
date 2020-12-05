f106489eb4efcdf5f95d266af3d1595706020335
SamirElKharrat <samirelkharrat@gmail.com>
2020-12-05 16:07:06 +0000

    Creamos el objeto Contacto en CV

--- src/main/java/dad/proyect/micv/model/CV.java
+++ src/main/java/dad/proyect/micv/model/CV.java
@@ -1,11 +1,13 @@
 package dad.proyect.micv.model;
 
+import dad.proyect.micv.model.Contacto.Contacto;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
 public class CV {
 
 	private ObjectProperty<Personal> personal = new SimpleObjectProperty<Personal>(new Personal());
+	private ObjectProperty<Contacto> contacto = new SimpleObjectProperty<Contacto>(new Contacto());
 
 	public final ObjectProperty<Personal> personalProperty() {
 		return this.personal;
@@ -18,5 +20,22 @@ public class CV {
 	public final void setPersonal(final Personal personal) {
 		this.personalProperty().set(personal);
 	}
+
+	public final ObjectProperty<Contacto> contactoProperty() {
+		return this.contacto;
+	}
+	
+
+	public final Contacto getContacto() {
+		return this.contactoProperty().get();
+	}
+	
+
+	public final void setContacto(final Contacto contacto) {
+		this.contactoProperty().set(contacto);
+	}
+	
+	
+	
 	
 }
